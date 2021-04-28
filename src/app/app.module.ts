0d33619139efbdebb2d168d3548b224398047489
MariemBADIS <mariem.badis@rhis-solutions.com>
2021-04-28 17:25:30 +0100

    create multi componenent

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { ChildAComponent } from './child-a/child-a.component';
+import { ChildBComponent } from './child-b/child-b.component';
+import { GrandChildComponent } from './grand-child/grand-child.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ChildAComponent,
+    ChildBComponent,
+    GrandChildComponent
   ],
   imports: [
     BrowserModule
