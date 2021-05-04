202bb9943f12dbfb857d3b340cf2640d43320136
MariemBADIS <mariem.badis@rhis-solutions.com>
2021-05-04 16:45:42 +0100

    sprint number 2 : create CV with detail (create model Personne)

--- src/app/cvCompany/cv/cv.component.ts
+++ src/app/cvCompany/cv/cv.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {Personne} from '../../Model/Personne';
 
 @Component({
   selector: 'app-cv',
@@ -7,9 +8,18 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CvComponent implements OnInit {
 
-  constructor() { }
+  personnes: Personne [];
+
+  constructor() {
+  }
 
   ngOnInit() {
+    this.personnes = [
+      new Personne(1, 'BADIS', 'Mariem', 30, '1.png', 1, 'developpeuse front end'),
+      new Personne(2, 'Allouchi', 'Idriss', 31, '2.png', 2, 'developpeuse front end'),
+      new Personne(3, 'Rihani', 'Salim', 36, '3.png', 3, 'developpeuse front end'),
+      new Personne(4, 'chaouch', 'Rym', 38, '4.png', 4, 'developpeuse front end')
+    ];
   }
 
 }
