ca3625200f967e2da942ef45f9c8c0a608047fae
MariemBADIS <mariem.badis@rhis-solutions.com>
2021-05-04 23:03:40 +0100

    directive : ngClass

---
+++ src/app/directive/class/class.component.ts
@@ -0,0 +1,38 @@
+import {Component, OnInit} from '@angular/core';
+
+@Component({
+  selector: 'app-class',
+  templateUrl: './class.component.html',
+  styleUrls: ['./class.component.css']
+})
+export class ClassComponent implements OnInit {
+
+  firstClass = true;
+  secondClass = false;
+  thirdClass = true;
+
+  constructor() {
+  }
+
+  ngOnInit() {
+  }
+
+  changeStyleBtn1() {
+    this.firstClass = true;
+    this.secondClass = false;
+    this.thirdClass = false;
+  }
+
+  changeStyleBtn2() {
+    this.firstClass = false;
+    this.secondClass = true;
+    this.thirdClass = false;
+  }
+
+  changeStyleBtn3() {
+    this.firstClass = false;
+    this.secondClass = false;
+    this.thirdClass = true;
+  }
+
+}
